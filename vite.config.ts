7dc6c40c8d77a2c0b993fea9061dc6428bd3497a
rovicBotones <62902249+rovicBotones@users.noreply.github.com>
2025-06-03 13:10:02 +0800

    remove dirname

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "app"),
+      "~": path.resolve("app"),
     },
   },
 });
